b859d88773d74381554afff6cd3123e6476e9f5b
xern0 <noblock@s.sfusd.edu>
2025-10-31 11:38:10 -0700

    Lesson 4 Completed

--- M08.start_a_dialog_workbook/lessons/dialogue.gd
+++ M08.start_a_dialog_workbook/lessons/dialogue.gd
@@ -11,67 +11,64 @@ var bodies := {
 	"pink": preload ("res://assets/pink.png")
 }
 
-## An array of dictionaries. Each dictionary has three properties:
+## An array of dictionaries. Each dictionary has four properties:
 ## - expression: a [code]Texture[/code] containing an expression
 ## - text: a [code]String[/code] containing the text the character says
-## - character: a [code]Texture[/code] representing the character
+## - character: a [code]Texture[/code] representing the character, that we extract from the [code]bodies[/code] dictionary
+## - choices: a [code]dictionary[/code] with [code]String[/code] keys and [code]int[/code] values
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
-		"text": "I've been learning about [wave]Arrays and Dictionaries[/wave]",
-		"character": bodies["sophia"]
-	},
-	{
-		"expression": expressions["regular"],
-		"text": "How has it been going?",
-		"character": bodies["pink"]
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "... Well... it is a little bit [shake]complicated[/shake]!",
-		"character": bodies["sophia"]
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "Oh!",
-		"character": bodies["pink"]
-	},
-	{
-		"expression": expressions["regular"],
-		"text": "I believe in you!",
-		"character": bodies["pink"]
+		"text": "[wave]Hey, wake up![/wave]\nIt's time to make video games.",
+		"character": bodies["sophia"],
+		"choices": {
+			"Let me sleep a little longer": 2,
+			"Let's do it!": 1,
+		},
 	},
 	{
 		"expression": expressions["happy"],
-		"text": "If you stick to it, you'll eventually make it!",
-		"character": bodies["pink"]
+		"text": "Great! Your first task will be to write a [b]dialogue tree[/b].",
+		"character": bodies["sophia"],
+		"choices": {
+			"I will do my best": 3,
+			"No, let me go back to sleep": 2,
+		},
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "Oh, come on! It'll be fun.",
+		"character": bodies["pink"],
+		"choices": {
+			"No, really, let me go back to sleep": 0,
+			"Alright, I'll try": 1,
+		},
 	},
 	{
 		"expression": expressions["happy"],
-		"text": "That's it! Let's [tornado freq=3.0][rainbow val=1.0]GOOOOOO!!![/rainbow][/tornado]",
-		"character": bodies["sophia"]
-	}
+		"text": "That's the spirit! [wave]You can do it![/wave]",
+		"character": bodies["pink"],
+		"choices": {"Okay! (Quit)": - 1},
+	},
 ]
-
 
 ## UI element that shows the texts
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
-## UI element that progresses to the next text
-@onready var next_button: Button = %NextButton
 ## Audio player that plays voice sounds while text is being written
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 ## The character
 @onready var body: TextureRect = %Body
 ## The Expression
 @onready var expression: TextureRect = %Expression
+## The container for buttons
+@onready var action_buttons_v_box_container: VBoxContainer = %ActionButtonsVBoxContainer
 
 
 func _ready() -> void:
 	show_text(0)
 
-
-
-## Draws the current text to the rich text element
+## Draws the selected text
+## [param current_item_index] Displays the currently selected index from the dialogue array
 func show_text(current_item_index: int) -> void:
 	# We retrieve the current item from the array
 	var current_item := dialogue_items[current_item_index]
@@ -81,7 +78,7 @@ func show_text(current_item_index: int) -> void:
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
 	body.texture = current_item["character"]
-
+	create_buttons(current_item["choices"])
 	# We set the initial visible ratio to the text to 0, so we can change it in the tween
 	rich_text_label.visible_ratio = 0.0
 	# We create a tween that will draw the text
@@ -106,13 +103,36 @@ func show_text(current_item_index: int) -> void:
 	# We animate the character sliding in.
 	slide_in()
 
-	# Finally, we disable the next button until the text finishes displaying.
+	# We disable the buttons, and re-enable them after the text is shown
+	for button: Button in action_buttons_v_box_container.get_children():
+		button.disabled = true
+	tween.finished.connect(func() -> void:
+		for button: Button in action_buttons_v_box_container.get_children():
+			button.disabled = false
+	)
 
-
-
-## Progresses to the next slide.
-	
-
+## Adds buttons to the buttons container
+## [param choices_data] A dictionary of [String] keys where each key represents
+##                      a sentence that the player can select, and each [int] value
+##                      represents a key for the next text item.
+func create_buttons(choices_data: Dictionary) -> void:
+	# We remove all previous buttons
+	for button in action_buttons_v_box_container.get_children():
+		button.queue_free()
+	# We loop over all the dictionary keys
+	for choice_text in choices_data:
+		var button := Button.new()
+		action_buttons_v_box_container.add_child(button)
+		button.text = choice_text
+		# We extract the target line index from the dictionary value
+		var target_line_idx: int = choices_data[choice_text]
+		if target_line_idx == - 1:
+			# If the target line index is -1, we want to quit
+			button.pressed.connect(get_tree().quit)
+		else:
+			# Otherwise we bind the target line index to the show_text function
+			# and use that in the pressed signal's connection
+			button.pressed.connect(show_text.bind(target_line_idx))
 
 ## Animates the character when they start talking
 func slide_in() -> void:
